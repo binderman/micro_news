95c63b13905530b6dde66857dba137bdad62329d
binderman <fbinder@gmail.com>
2023-06-27 13:39:25 -0300

    refactor: extract cors headers function

--- backends/python/micro_news.py
+++ backends/python/micro_news.py
@@ -8,16 +8,19 @@ CORS(app)
 API_KEY = '8dade0feadcf43e285e215cc7271de9c'
 URL = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
 
+def add_cors_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'GET')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    return response
+
 @app.route('/api/getArticle/<title>', methods=['GET'])
 def get_article(title):
     response = requests.get(URL)
     data = response.json()
     article = next((a for a in data['articles'] if a['title'] == title), None)
     response = jsonify(article)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    return response
+    return add_cors_headers(response)
 
 @app.route('/api/getAuthorArticles/<author>', methods=['GET'])
 def get_author_articles(author):
@@ -25,19 +28,13 @@ def get_author_articles(author):
     data = response.json()
     articles = [a for a in data['articles'] if a['author'] == author]
     response = jsonify(articles)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    return response
+    return add_cors_headers(response)
 
 @app.route('/api/getTopHeadlines', methods=['GET'])
 def get_top_headlines():        
     response = requests.get(f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}')
     response = jsonify(response.json())
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    return response
+    return add_cors_headers(response)
 
 if __name__ == '__main__':
     app.run(port=8080)
